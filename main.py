4effe456ffee0f5b82e951f525377653bcd14812
staskoko <117083501+staskoko@users.noreply.github.com>
2025-02-20 09:54:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -155,7 +155,7 @@ class AUTOENCODER(nn.Module):
 model = AUTOENCODER(Num_meas, Num_Obsv, Num_Neurons).to(device)
 
 # Training loop variables
-eps = 200         # Number of epochs per batch size
+eps = 2         # Number of epochs per batch size
 lr = 1e-4         # Learning rate
 batch_size = 256
 S_p = 30
