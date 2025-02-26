6ba2da8843239fcd2159dcf4bd46d2baeda6dbec
Berako00 <126508321+Berako00@users.noreply.github.com>
2025-02-26 11:28:46 +0100

    Update Data_Generation.py

--- Data_Generation.py
+++ Data_Generation.py
@@ -120,15 +120,12 @@ def DataGenerator(x1range, x2range, numICs, tSpan, mu, lam, file_path, type):
       # Create test, validation, and training tensors with different percentages of numICs
       seed = 1
       test_tensor = DiscreteSpectrumExampleFn(x1range, x2range, round(0.1 * numICs), tSpan, mu, lam, seed)
-      print("Test tensor shape:", test_tensor.shape)  # Expected: [0.1*numICs, len(tSpan), 2]
 
       seed = 2
       val_tensor = DiscreteSpectrumExampleFn(x1range, x2range, round(0.2 * numICs), tSpan, mu, lam, seed)
-      print("Validation tensor shape:", val_tensor.shape)  # Expected: [0.2*numICs, len(tSpan), 2]
 
       seed = 3
       train_tensor = DiscreteSpectrumExampleFn(x1range, x2range, round(0.7 * numICs), tSpan, mu, lam, seed)
-      print(f"Training tensor shape (seed {seed}):", train_tensor.shape)
     
     else:
       train_tensor, val_tensor, test_tensor = tensor_prep(file_path)
