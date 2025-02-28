680ba6ea99e7515ab2e0e89ed8a990201883cd98
Berako00 <126508321+Berako00@users.noreply.github.com>
2025-02-28 13:54:58 +0100

    Update loss_func.py

--- loss_func.py
+++ loss_func.py
@@ -11,7 +11,6 @@ def loss_recon(xk, phi, phi_inv): # inputs (xk,encoder,decoder)
     return recon_loss
 
 def loss_pred(xk, S_p, K, phi, phi_inv): # inputs (xk, Sp = 30, Koopman, encoder, decoder)
-
     total_loss = torch.tensor(0.0, device=xk[:, 0, :].device)
     pred_loop = phi(xk[:, 0, :])
     for m in range(1, S_p + 1):
@@ -20,42 +19,28 @@ def loss_pred(xk, S_p, K, phi, phi_inv): # inputs (xk, Sp = 30, Koopman, encoder
         total_loss += F.mse_loss(pred, xk[:, m, :], reduction='mean')
 
     pred_loss = total_loss / S_p
-    #print(f"Predicted loss: {pred_loss}")
-
     return pred_loss
 
 def loss_lin(xk, T, K, phi, phi_inv):# inputs (xk, Koopman, encoder, decoder)
-    #T = len(xk[0, :, :])
     total_loss = torch.tensor(0.0, device=xk[:, 0, :].device)
     pred_loop = phi(xk[:, 0, :])
-
     for m in range(1, T - 1):
         pred_loop = K(pred_loop)
         actual = phi(xk[:, m, :])
-
         total_loss += F.mse_loss(pred_loop, actual, reduction='mean')
-
     lin_loss = total_loss / (T - 1)
-    #print(f"Linear loss: {lin_loss}")
-
     return lin_loss
 
 def loss_inf(xk, K, phi, phi_inv):
-
-    #first_term = loss_recon(xk, phi, phi_inv).abs().max()
-    #second_term = loss_pred(xk, 1, K, phi, phi_inv).abs().max()
-
     x1 = xk[:, 0, :]
     recon_pred = phi_inv(phi(x1))
     first_term = (x1 - recon_pred).abs().max(dim=1)[0].mean()
-
+    
     one_step_latent = K(phi(x1))
     one_step_pred = phi_inv(one_step_latent)
     second_term = (xk[:, 1, :] - one_step_pred).abs().max(dim=1)[0].mean()
-
+    
     inf_loss = first_term + second_term
-    #print(f"Inf loss: {inf_loss}")
-
     return inf_loss
 
 def total_loss(alpha, W, xk, S_p, T, K, phi, phi_inv): # inputs (alpha, W, xk, Sp = 30, Koopman, encoder, decoder)
@@ -70,7 +55,6 @@ def total_loss(alpha, W, xk, S_p, T, K, phi, phi_inv): # inputs (alpha, W, xk, S
     L_inf = loss_inf(xk, K, phi, phi_inv)
 
     L_total = alpha_1*(L_recon + L_pred) + L_lin + alpha_2*L_inf # + alpha_3*W_norm_sqr
-    #print(f"Total loss: {L_total}")
 
     return L_total, L_recon, L_pred, L_lin, L_inf
 
